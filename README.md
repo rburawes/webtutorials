04b496980c787f8128a634f8af0132d9c5e6d960
rburawes <rburawes@gmail.com>
2014-10-15 22:27:45 +0800

    initial commit for web tutorials

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ webtutorials
 ============
 
 Sample web applications tutorials.
+
+How to run:
+
+1. You can use any web application server e.g. tomcat.
+2. Put the project on your tomcat's webapps.
+3. Run tomcat.
+4. Access http://localhost:8083/WebSample/SampleServlet on your browser.
---
+++ WebSample/src/com/sample/bean/Book.java
@@ -0,0 +1,25 @@
+package com.sample.bean;
+
+public class Book {
+	
+	private String name;
+	
+	private int edition;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName( String name ) {
+		this.name = name;
+	}
+
+	public int getEdition() {
+		return edition;
+	}
+
+	public void setEdition( int edition ) {
+		this.edition = edition;
+	}
+	
+}
